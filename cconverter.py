cc9568f274c78be496e6c73f4761e1c09ee2bd6c
Gavin Thomson <gavin.r.thomson@gmail.com>
2022-09-19 12:45:24 +0100

    fixed comments

--- cconverter.py
+++ cconverter.py
@@ -1,15 +1,23 @@
-# write your code here!
-
 import requests
 import json
 
+#inputcurr is what we're selling
+
 inputcurr = str.lower(input())
 
+#fetch json record for inputcurr
+
 rates = requests.get("https://floatrates.com/daily/" + inputcurr + ".json")
-# print(r.text)
+
+#parse to .text
+
 parsedrates = json.loads(rates.text)
 
+#initialise empty cache
+
 ratescache = {}
+
+# if inputcurr is not usd or eur, add inputcurr/usd and inputcurr/eur rates to cache
 
 if inputcurr.lower() != "usd":
     usdrecord = parsedrates["usd"]
@@ -20,11 +28,9 @@ if inputcurr.lower() != "eur":
     eurrate = eurrecord["rate"]
     ratescache["eur"] = eurrate
 
-# BEGINNING OF LOOP
-
 while True:
-    outputcurr = str.lower(input())
-    if outputcurr == '':
+    outputcurr = str.lower(input())  #outputcurr is what we're buying
+    if outputcurr == '':             #break and exit on a blank outputcurr
         break
     
     inputcoins = float(input())
@@ -38,9 +44,8 @@ while True:
         outputrecord = parsedr[outputcurr]
         newrate = outputrecord["rate"]
         ratescache[outputcurr] = newrate
-    
+    #if outputcurr not a key in cache, fetch and parse rate from json and store as new key:value pair
     moneyreceived = round(inputcoins * ratescache[outputcurr], 2)
     print(f"You received {moneyreceived} {outputcurr.upper()}.")
 
-# END OF LOOP
 
